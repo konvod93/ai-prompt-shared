f2f9d4bc183fafa8473eb6bc7ab5c0e2444c7837
Konstantin <57582571+konvod93@users.noreply.github.com>
2023-11-18 20:57:13 +0200

    created file structure of components

--- app/page.jsx
+++ app/page.jsx
@@ -12,6 +12,7 @@ const Home = () => {
             Promptopia is an open source AI prompting tool for modern world to 
             discover, create and share creative prompts
         </p>
+        {/* Feed */}
     </section>
   )
 }
