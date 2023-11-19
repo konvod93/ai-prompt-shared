85e113718e4a5549cc31a3f4d707f3aa40b74cec
Konstantin <57582571+konvod93@users.noreply.github.com>
2023-11-19 12:30:16 +0200

    import and added Feed to homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import Feed from "@components/Feed";
 
 const Home = () => {
   return (
@@ -12,7 +12,7 @@ const Home = () => {
             Promptopia is an open source AI prompting tool for modern world to 
             discover, create and share creative prompts
         </p>
-        {/* Feed */}
+        <Feed />
     </section>
   )
 }
