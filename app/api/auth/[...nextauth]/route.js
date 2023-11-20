3074a746710a1f3d5fe05c0f911a503122292d13
Konstantin <57582571+konvod93@users.noreply.github.com>
2023-11-20 18:54:08 +0200

    created user schema, edited signIn function

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import User from "@models/user";
 import { connectToDB } from "@utils/database";
 
 
@@ -17,7 +18,19 @@ const handler = NextAuth({
         try {
             await connectToDB();
             // check if a user already exists
+
+            const userExists = await User.findOne({
+                email: profile.email
+            })
             // if not, create a new user
+
+            if (!userExists) {
+                await User.create({
+                    email: profile.email,
+                    username: profile.name.replace(" ", "").toLowerCase(),
+                    image: profile.picture
+                })
+            }
             
             return true;
         } catch (error) {
