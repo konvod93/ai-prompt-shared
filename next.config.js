36d09d717fc83ce4c5b62f9cfebd3d2eec4cb87e
Konstantin <57582571+konvod93@users.noreply.github.com>
2023-11-21 19:50:49 +0200

    added user image in Nav

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        serverComponentsExternalPackages: ["mongoose"],
-      }      
-}
+  experimental: {
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    domains: ["lh3.googleusercontent.com"],
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
+    return config;
+  },
+};
 
 module.exports = nextConfig
